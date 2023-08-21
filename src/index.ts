15a11076f14c41a973d7bae4d739dd66c386baca
Gullbra <martingullbrandssonmoreira@hotmail.com>
2023-08-21 18:31:03 +0200

    import of readline after updating node from 16 to 18

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,37 @@
-import { IJsonData } from "./interfaces"
 const dataReader = (await import(process.env.MODE === "ts-node" ? './dataReader.ts' : './dataReader')).dataReader as () => Promise<IJsonData>
+import { IJsonData } from "./interfaces"
+import * as readline from 'node:readline/promises';
+import { stdin, stdout } from 'node:process';
+
+const rl = readline.createInterface({ input: stdin, output: stdout });
 
 const wordData = await dataReader()
+let workingData = wordData
+let terminaInput: string;
 
-console.log("hey", Object.keys(wordData))
+const inputProcessor = (inputString: string): string => inputString.toLowerCase().trim()
+
+console.log("\n--------------------------")
+console.log("Deutch lernen, commandline")
+console.log("--------------------------\n")
+
+const startUp = async () => {
+  terminaInput = inputProcessor(await rl.question('Start review (Y)?  ')); 
+  
+  if (terminaInput === 'y') {
+    console.log("\nGut! Lass uns anfangen!")
+  } else {
+    console.log("quit?")
+  }
+
+
+  rl.close()
+}
+
+await startUp()
+
+
+
+//console.log("hey", Object.keys(wordData))
 
 //export {}
