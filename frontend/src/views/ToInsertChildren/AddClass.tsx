51d793d9f3eab9ea144c0066ac851188acbd8324
Gullbra <martingullbrandssonmoreira@hotmail.com>
2023-11-08 19:38:57 +0100

    barebones noun form

--- frontend/src/views/ToInsertChildren/AddClass.tsx
+++ frontend/src/views/ToInsertChildren/AddClass.tsx
@@ -1,7 +1,10 @@
 import { useNavigate, useOutletContext } from "react-router-dom"
 import { IOutletContext } from "../../interfaces/IStatesAndContexts"
 import { useEffect, useState } from "react"
-import { IClassNoun, TUnknownWordClass } from "../../interfaces/wordsPhrasesGrammar"
+import { IClassNoun, INounDeclension, TUnknownWordClass } from "../../interfaces/wordsPhrasesGrammar"
+import { capitalizeWord } from "../../util/capitalize"
+
+import '../../styles/views/toInsertChildren/forms.css'
 
 export const AddClassView = () => {
   const navigate = useNavigate()
@@ -13,7 +16,13 @@ export const AddClassView = () => {
     translation: [],
     article: '',
     plural: '',
-    specialDeclensions: {}
+    specialDeclensions: {
+      nGeneral: false,
+      enGeneral: false,
+      nsGenitiv: false,
+      sesGenetiv: false,
+      esGenetivForced: false
+    }
   });
 
 
@@ -21,6 +30,18 @@ export const AddClassView = () => {
     if(!wordInEdit)
       navigate('/ToInsert')
   }, [])
+
+
+  const formInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+
+    if (name === 'translation') {
+      setFormData({ ...formData, [name]: value.split(',') })
+      return
+    }
+
+    setFormData({ ...formData, [name]: value });
+  }
 
 
   const formSubmit = (event: React.FormEvent<HTMLFormElement>)  => {
@@ -70,9 +91,9 @@ export const AddClassView = () => {
 
 
       <form onSubmit={formSubmit}>
-
-
-
+        {
+          wordClassType === 'noun' && (<FormInputsNoun formData={formData} formInputChange={formInputChange} setFormData={setFormData}/>)
+        }
 
         <button type="submit">Add class and return to overview</button>
         <button onClick={formCancel}>Cancel</button>
@@ -82,6 +103,68 @@ export const AddClassView = () => {
       {wordInEdit?.classes && wordInEdit?.classes?.length > 0 && (
         <button onClick={endEditing}>Finish</button>
       )}
+    </>
+  )
+}
+
+const FormInputsNoun = (
+  { formData, formInputChange, setFormData }: 
+  { formData: IClassNoun, formInputChange: (event: React.ChangeEvent<HTMLInputElement>) => void, setFormData: React.Dispatch<React.SetStateAction<IClassNoun>> }
+) => {
+  const elementsArr = [
+    {
+      type: "text",
+      name: "article",
+      value: formData.article
+    },
+    {
+      type: "text",
+      name: "plural",
+      value: formData.plural
+    },
+    {
+      type: "text",
+      name: "translation",
+      value: formData.translation
+    },
+  ]
+
+
+  const formDeclInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, checked } = event.target;
+    setFormData({ ...formData, specialDeclensions: {...formData.specialDeclensions, [name]: checked }});
+  }
+
+
+  const generateDeclentionBoxes = () => {
+    const elArr = []
+
+    for (const [key, value] of Object.entries(formData.specialDeclensions)) {
+      elArr.push(
+        <label className='form__label--checkbox'>
+          <input type='checkbox' name={key} checked={value} onChange={formDeclInputChange}/>  
+          {key +': '}
+        </label>
+      )
+    }
+
+    return (<>{elArr}</>)
+  }
+
+
+  return (
+    <>
+      {elementsArr.map(el => (
+        <label className='form__label'>
+          {capitalizeWord(el.name) +': '}
+          <input key={el.name} type={el.type} name={el.name} value={el.value} onChange={formInputChange}/>  
+        </label>
+      ))}
+
+      <div className="form__decl-inputs-container dev-border">
+        <p className="declention-section-header">Special Declensions</p>
+        {generateDeclentionBoxes()}
+      </div>
     </>
   )
 }
